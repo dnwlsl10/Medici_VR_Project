02561d18024cbb395096b80670e60d261898838e
DESKTOP-DK5CVFT\medici <youngho0983@naver.com>
2022-04-20 16:11:13 +0900

    새로운 프로젝트로 옮기기 , UIManager , ObejctPool ,  버튼 폭탄

---
+++ Assets/Base/Scripts/Bomb/BombManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombManager : MonoBehaviour
+{
+    public static BombManager instance;
+
+    public bool isFail;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFail = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( isFail )
+        {
+            print("실패코드");
+        }
+    }
+}
